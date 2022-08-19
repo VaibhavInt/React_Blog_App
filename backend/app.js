1dfd4adec7039f5a58ef76270ddac214c82d3029
VaibhavInt <vaibhavshukla2704@gmail.com>
2022-08-19 17:31:05 +0530

    done some more work on backend

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,19 @@
 import express from "express";
 import mongoose from "mongoose";
+import router from "./routes/user-routes";
 
 const app = express();
 
-mongoose.connect('mongodb+srv://admin:admin@cluster0.r1scmoc.mongodb.net/Blog?retryWrites=true&w=majority').then(()=>app.listen(5000)).then(()=>console.log("Connected to Database and Listening to LocalHost 5000")).catch((err)=>console.log(err));
+app.use("/api/user", router);
 
+mongoose
+  .connect(
+    "mongodb+srv://admin:admin@cluster0.r1scmoc.mongodb.net/Blog?retryWrites=true&w=majority"
+  )
+  .then(() => app.listen(5000))
+  .then(() =>
+    console.log("Connected to Database and Listening to LocalHost 5000")
+  )
+  .catch((err) => console.log(err));
 
 // app.listen(5000);
---
+++ backend/controllers/user-controller.js
@@ -0,0 +1,24 @@
+import User from "../model/User";
+
+export const getAllUser = async (req, res, next) => {
+  let users;
+  try {
+    users = await User.find();
+  } catch (err) {
+    console.log(err);
+  }
+  if (!users) {
+    return res.status(404).json({ message: "No Users Found" });
+  }
+  return res.status(200).json({ users });
+};
+
+export const signup = async (req, res, next) => {
+  const { name, email, password } = req.body;
+
+  let existingUser;
+  try {
+  } catch (err) {
+    console.log(err);
+  }
+};
