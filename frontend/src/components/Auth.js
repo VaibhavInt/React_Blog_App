d98792bc9623ffba6ab9345ae042425984641891
Vaibhav <vaibhavshukla2704@gmail.com>
2022-08-25 18:52:04 +0530

    completed login page

--- frontend/src/components/Auth.js
+++ frontend/src/components/Auth.js
@@ -1,7 +1,101 @@
-import React from "react";
+import { Box, Button, TextField, Typography } from "@mui/material";
+import React, { useState } from "react";
+import axios from "axios";
 
 const Auth = () => {
-  return <div>Auth</div>;
+  const [inputs, setInputs] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const [isSignup, setIsSignup] = useState(false);
+
+  const handleChange = (e) => {
+    setInputs((prevState) => ({
+      ...prevState,
+      [e.target.name]: [e.target.value],
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("va", inputs);
+    sendRequest();
+  };
+
+  const sendRequest = async () => {
+    const res = await axios
+      .post("http://localhost:5000/api/user/login", {
+        email: inputs.email,
+        password: inputs.password,
+      })
+      .catch((err) => console.log(err));
+
+    const data = await res.data;
+    return data;
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <Box
+          maxWidth={400}
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="center "
+          boxShadow="10px 10px 20px #ccc"
+          padding={3}
+          margin="auto"
+          marginTop={5}
+          borderRadius={5}
+        >
+          <Typography variant="h4" padding={3} textAlign="center">
+            {isSignup ? "Signup" : "Login"}
+          </Typography>
+          {isSignup && (
+            <TextField
+              name="name"
+              onChange={handleChange}
+              value={inputs.name}
+              placeholder="Name"
+              margin="normal"
+            />
+          )}
+          <TextField
+            name="email"
+            onChange={handleChange}
+            value={inputs.email}
+            type={"email"}
+            placeholder="Email"
+            margin="normal"
+          />
+          <TextField
+            name="password"
+            onChange={handleChange}
+            value={inputs.password}
+            type={"password"}
+            placeholder="Password"
+            margin="normal"
+          />
+          <Button
+            type="submit"
+            variant="contained"
+            sx={{ borderRadius: 3, marginTop: 3 }}
+            color="warning"
+          >
+            Submit
+          </Button>
+          <Button
+            onClick={() => setIsSignup(!isSignup)}
+            sx={{ borderRadius: 3, marginTop: 3 }}
+          >
+            Change to {isSignup ? "Login" : "Signup"}
+          </Button>
+        </Box>
+      </form>
+    </div>
+  );
 };
 
 export default Auth;
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -8,8 +8,12 @@ import {
   Toolbar,
   Typography,
 } from "@mui/material";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const isLoggedIn = useSelector((state) => state.isLoggedIn);
+
   const [value, setValue] = useState();
 
   return (
@@ -22,39 +26,53 @@ const Header = () => {
     >
       <Toolbar>
         <Typography variant="h4">BlogsApp</Typography>
-        <Box display="flex" marginLeft={"auto"} marginRight={"auto"}>
-          <Tabs
-            textColor="inherit"
-            value={value}
-            onChange={(e, val) => setValue(val)}
-          >
-            <Tab label="All Blogs" />
-            <Tab label="My Blogs" />
-          </Tabs>
-        </Box>
-        <Box display="flex" marginLeft="auto">
+        {isLoggedIn && (
+          <Box display="flex" marginLeft={"auto"} marginRight={"auto"}>
+            <Tabs
+              textColor="inherit"
+              value={value}
+              onChange={(e, val) => setValue(val)}
+            >
+              <Tab LinkComponent={Link} to="/blogs" label="All Blogs" />
+              <Tab LinkComponent={Link} to="/myBlogs" label="My Blogs" />
+            </Tabs>
+          </Box>
+        )}
+        {!isLoggedIn && (
+          <>
+            <Box display="flex" marginLeft="auto">
+              <Button
+                LinkComponent={Link}
+                to="/auth"
+                variant="contained"
+                sx={{ margin: "4px", borderRadius: 8 }}
+                color="warning"
+              >
+                Login{" "}
+              </Button>
+              <Button
+                LinkComponent={Link}
+                to="/auth"
+                variant="contained"
+                sx={{ margin: "4px", borderRadius: 8 }}
+                color="warning"
+              >
+                Signup{" "}
+              </Button>
+            </Box>{" "}
+          </>
+        )}
+        {isLoggedIn && (
           <Button
-            variant="contained"
-            sx={{ margin: "4px", borderRadius: 8 }}
-            color="warning"
-          >
-            Login{" "}
-          </Button>
-          <Button
-            variant="contained"
-            sx={{ margin: "4px", borderRadius: 8 }}
-            color="warning"
-          >
-            Signup{" "}
-          </Button>
-          <Button
+            LinkComponent={Link}
+            to="/auth"
             variant="contained"
             sx={{ margin: "4px", borderRadius: 8 }}
             color="warning"
           >
             Logout{" "}
           </Button>
-        </Box>
+        )}
       </Toolbar>
     </AppBar>
   );
